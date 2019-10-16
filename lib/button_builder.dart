536449b29e0f2ddb383d145210b8461a5f44a1ac
Tariq <tariq.abusheikh@gmail.com>
2019-10-16 13:50:15 -0500

    `SignInButtonBuilder` enhancments:  * Add optional parameter `image`  * Add optional parameter `fontSize`  * Add optional parameter `height`  * `icon` OR `image` are now required  * Split widget building logic into separate functions

--- lib/button_builder.dart
+++ lib/button_builder.dart
@@ -8,11 +8,20 @@ class SignInButtonBuilder extends StatelessWidget {
   /// User can use Flutter built-in Icons or font-awesome flutter's Icon
   final IconData icon;
 
+  /// Override the icon section with a image logo
+  /// For example, Google requires a colorized logo,
+  /// which FontAwesome cannot display. If both image
+  /// and icon are provided, image will take precedence
+  final Widget image;
+
   /// `mini` tag is used to switch from a full-width signin button to
   final bool mini;
 
   /// the button's text
   final String text;
+
+  /// The size of the label font
+  final double fontSize;
 
   /// backgroundColor is required but textColor is default to `Colors.white`
   /// splashColor is defalt to `Colors.white30`
@@ -32,25 +41,31 @@ class SignInButtonBuilder extends StatelessWidget {
   /// elevation has defalt value of 2.0
   final double elevation;
 
+  /// the height of the button; defaults to
+  final double height;
+
   /// width is default to be 1/1.5 of the screen
   final double width;
 
   /// The constructor is self-explanatory.
   SignInButtonBuilder({
     Key key,
-    @required this.icon,
     @required this.backgroundColor,
     @required this.onPressed,
     @required this.text,
+    this.icon,
+    this.image = null,
+    this.fontSize = 14.0,
     this.textColor = Colors.white,
     this.splashColor = Colors.white30,
     this.padding = const EdgeInsets.all(3.0),
     this.mini = false,
     this.elevation = 2.0,
     this.shape,
+    this.height = null,
     this.width = null,
   })  : assert(text != null),
-        assert(icon != null),
+        assert(icon != null || image != null),
         assert(textColor != null),
         assert(backgroundColor != null),
         assert(onPressed != null),
@@ -62,48 +77,64 @@ class SignInButtonBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialButton(
       key: key,
-      minWidth: mini ? 35.0 : null,
+      minWidth: mini ? width ?? 35.0 : null,
+      height: height,
       elevation: elevation,
       padding: padding,
       color: backgroundColor,
       onPressed: onPressed,
       splashColor: splashColor,
-      child: mini
-          ? Container(
-              width: 35.0,
-              height: 35.0,
-              child: Icon(
-                icon,
-                color: Colors.white,
-              ),
-            )
-          : Container(
-              constraints: BoxConstraints(
-                maxWidth: width ?? MediaQuery.of(context).size.width / 1.5,
-              ),
-              child: Center(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width / 20),
-                      child: Icon(
-                        icon,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      text,
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        color: textColor,
-                      ),
-                    ),
-                  ],
-                ),
+      child: _getButtonChild(context),
+      shape: shape ?? ButtonTheme.of(context).shape,
+    );
+  }
+
+  /// Get the inner content of a button
+  Container _getButtonChild(BuildContext context) {
+    if (mini) {
+      return Container(
+        width: height ?? 35.0,
+        height: width ?? 35.0,
+        child: _getIconOrImage(),
+      );
+    }
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: width ?? MediaQuery.of(context).size.width / 1.5,
+      ),
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: padding ??
+                  EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width / 20,
+                  ),
+              child: _getIconOrImage(),
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                color: textColor,
+                fontSize: fontSize,
               ),
             ),
-      shape: shape ?? ButtonTheme.of(context).shape,
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Get the icon or image widget
+  Widget _getIconOrImage() {
+    if (image != null) {
+      return image;
+    }
+    return Icon(
+      icon,
+      color: Colors.white,
     );
   }
 }
